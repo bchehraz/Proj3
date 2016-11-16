0bda0703ccb28aa054853de7432c737eb6510def
uncleurdnot <qminora@gmail.com>
2016-11-16 11:45:02 -0800

    Initial push with pyaudio

--- Proj3.py
+++ Proj3.py
@@ -1,5 +1,23 @@
-import kivy
-import pyAudioAnalysis
-kivy.require("1.9.0")
+import scipy
+import pyaudio
+import wave
+import os, sys
 
-print("I LOVE THE MEMES GIVE ME THE MEMES")
+CHUNK = 1024
+
+wavfile = wave.open("09 Darling, Mommy is Dead.wav", 'rb')
+s1 = pyaudio.PyAudio()
+stream1 = s1.open(format=s1.get_format_from_width(wavfile.getsampwidth()),
+                channels=wavfile.getnchannels(),
+                rate=wavfile.getframerate(),
+                output=True)
+data = wavfile.readframes(CHUNK)
+#Play
+while len(data) > 0:
+    stream1.write(data)
+    data = wavfile.readframes(CHUNK)
+#Terminate
+stream.stop_stream()
+stream.close()
+
+
