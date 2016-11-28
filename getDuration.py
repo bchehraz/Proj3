2b80b80ffcfadc2183059a09c370c4775124c5bd
Babak Chehraz <bchehraz@csumb.edu>
2016-11-28 11:41:53 -0800

    modified getduration to handle file not found

--- getDuration.py
+++ getDuration.py
@@ -3,4 +3,7 @@ from kivy.core.audio import SoundLoader
 def getDuration(filename):
 	sound = SoundLoader.load(filename)
 	if sound:
-		return sound.length;
+		return sound.length
+	else
+		print ("-- get duration failed - file not found")
+		return -1
