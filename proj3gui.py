9488dcb6522571b0bb7ba276533812b3ba7b4584
Shaikh <ssultani@csumb.edu>
2016-12-11 12:08:48 -0800

    Adding Dropdown menu to the gui

--- proj3gui.py
+++ proj3gui.py
@@ -4,6 +4,10 @@ from kivy.app import App
 from kivy.lang import Builder 
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.config import Config
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button 
+from kivy.base import runTouchApp 
+
 Config.set('graphics', 'width', 600)
 Config.set('graphics', 'height', 300)
 
@@ -12,14 +16,29 @@ Builder.load_file('sound.kv')
 Builder.load_file('soundanalysis.kv')
 
 class Project(AnchorLayout):
-	pass
+    pass
 
 class ProjectApp(App):
-	def build(self):
-		return Project()
+    def build(self): 
+       return Project()
+
+dropdown = DropDown()
+tuningmenu = ['E Standard', 'Drop D', 'Half Step Down', 'Drop Db', 'D Standard', 'Drop C', 'C Standard']
+for tuning in tuningmenu:
+    btn = Button(text=str(tuning), size_hint_y=None, height=44)
+    btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+    dropdown.add_widget(btn)
+
+mainbutton = Button(text='E Standard', size_hint=(None, None))
+mainbutton.bind(on_release=dropdown.open)
+dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+
+'''runTouchApp(mainbutton)'''
 
 if __name__=="__main__": 
-	ProjectApp().run()
+    ProjectApp().run()
+
+
 
 
 
